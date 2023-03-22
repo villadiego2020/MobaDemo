b023514a11c0c30a00e6f67f59ff7886b4c93e76
Garvil Villadiego <Garvil.Personal@gmail.com>
2023-03-22 10:56:34 +0700

    Update Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start");
     }
 
     // Update is called once per frame
